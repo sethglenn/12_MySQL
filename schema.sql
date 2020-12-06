b6fc51c7e20c8117b0214dd508a192585f92d583
Seth Glenn <Sbgees@yahoo.com>
2020-12-06 12:23:11 -0600

    beginning database creation and file structure

---
+++ schema.sql
@@ -0,0 +1,16 @@
+DROP DATABASE IF EXISTS employees_db;
+
+CREATE DATABASE employees_db;
+
+USE employees_db;
+
+CREATE TABLE department (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(30) NOT NULL
+);
+
+CREATE TABLE role (
+
+
+
+);
