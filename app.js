2266997bebd1bae6c019f9302268b9d0f7b3e36d
Seth Glenn <Sbgees@yahoo.com>
2020-12-06 16:07:43 -0600

    installed dependencies and created server file

---
+++ app.js
@@ -0,0 +1,16 @@
+const mysql = require('mysql');
+const inquirer = require('inquirer');
+
+var connection = mysql.createConnection({
+    host: "localhost",
+  
+    // Your port; if not 3306
+    port: 3306,
+  
+    // Your username
+    user: "root",
+  
+    // Your password
+    password: "password",
+    database: "employees_db"
+  });
--- schema.sql
+++ schema.sql
@@ -10,7 +10,18 @@ CREATE TABLE department (
 );
 
 CREATE TABLE role (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL(10,2) NOT NULL,
+    department_id INT
 
+);
 
+CREATE TABLE employee (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT NOT NULL,
+    manager_id INT NOT NULL,
 
 );
