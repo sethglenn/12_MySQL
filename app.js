ca5747f1ab76ab71b414324ba897c09608289b48
Seth Glenn <Sbgees@yahoo.com>
2020-12-06 20:06:58 -0600

    creating logic functions to handle requests to database

--- app.js
+++ app.js
@@ -35,4 +35,37 @@ function initPrompt() {
             ]
 
         })
+        .then(function (answer) {
+
+
+
+        })
+
+
+};
+
+function searchDept(){
+    connection.query("SELECT * from department", function (err, res) {
+        if (err) throw err;
+        console.table(res);
+        initPrompt();
+    })
+}
+
+function viewAll(){
+    connection.query("SELECT employee.employee_id, employee.first_name, employee.last_name, role.title, department.name AS department, role.salary, CONCAT(manager.first_name, ' ', manager.last_name) AS manager FROM employee LEFT JOIN role on employee.role_id = role.role_id LEFT JOIN department on role.department_id = department.department_id LEFT JOIN employee manager on manager.manager_id = employee.manager_id;",
+        function(err, res) {
+            if (err) throw err;
+            console.table(res);
+            initPrompt();
+        }
+    );
+};
+
+function searchEmp(){
+    connection.query("SELECT * from role", function(err, res){
+        if (err) throw err;
+        console.table(res);
+        initPrompt();
+    })
 }
